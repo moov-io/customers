ade276b9b7fc66cc450da918233358c321a247c9
Adam Shannon <adamkshannon@gmail.com>
2019-05-09 18:31:10 -0500

    cmd/server: add initial HTTP server and Docker image

---
+++ version.go
@@ -0,0 +1,7 @@
+// Copyright 2019 The Moov Authors
+// Use of this source code is governed by an Apache License
+// license that can be found in the LICENSE file.
+
+package customers
+
+const Version = "v0.0.0-dev"
