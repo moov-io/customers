e666b05231592c8eadd3364f8fe0d439ba8691ba
Adam Shannon <adamkshannon@gmail.com>
2019-05-15 14:54:23 -0500

    cmd/server: copy over initial Customer routes

---
+++ cmd/server/customers.go
@@ -0,0 +1,321 @@
+// Copyright 2019 The Moov Authors
+// Use of this source code is governed by an Apache License
+// license that can be found in the LICENSE file.
+
+package main
+
+import (
+	"database/sql"
+	"encoding/json"
+	"errors"
+	"fmt"
+	"net/http"
+	"strings"
+	"time"
+
+	"github.com/moov-io/base"
+	moovhttp "github.com/moov-io/base/http"
+	client "github.com/moov-io/customers/client"
+
+	"github.com/go-kit/kit/log"
+	"github.com/gorilla/mux"
+)
+
+var (
+	errNoCustomerId = errors.New("no Customer ID found")
+)
+
+func addCustomerRoutes(logger log.Logger, r *mux.Router, repo customerRepository) {
+	r.Methods("GET").Path("/customers/{customerId}").HandlerFunc(getCustomer(logger, repo))
+	r.Methods("POST").Path("/customers").HandlerFunc(createCustomer(logger, repo))
+}
+
+func getCustomerId(w http.ResponseWriter, r *http.Request) string {
+	v, ok := mux.Vars(r)["customerId"]
+	if !ok || v == "" {
+		moovhttp.Problem(w, errNoCustomerId)
+		return ""
+	}
+	return v
+}
+
+func getCustomer(logger log.Logger, repo customerRepository) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		w = wrapResponseWriter(logger, w, r)
+
+		cust, err := repo.getCustomer(getCustomerId(w, r))
+		if err != nil {
+			if requestId := moovhttp.GetRequestId(r); requestId != "" {
+				logger.Log("accounts", fmt.Sprintf("%v", err), "requestId", requestId)
+			}
+			moovhttp.Problem(w, err)
+			return
+		}
+
+		w.Header().Set("Content-Type", "application/json; charset=utf-8")
+		w.WriteHeader(http.StatusOK)
+		json.NewEncoder(w).Encode(cust)
+	}
+}
+
+type customerRequest struct {
+	FirstName  string    `json:"firstName"`
+	MiddleName string    `json:"middleName"`
+	LastName   string    `json:"lastName"`
+	NickName   string    `json:"nickName"`
+	Suffix     string    `json:"suffix"`
+	BirthDate  time.Time `json:"birthDate"`
+	Email      string    `json:"email"`
+	SSN        string    `json:"SSN"`
+	Phones     []phone   `json:"phones"`
+	Addresses  []address `json:"addresses"`
+}
+
+type phone struct {
+	Number string `json:"number"`
+	Type   string `json:"type"`
+}
+
+type address struct {
+	Address1   string `json:"address1"`
+	Address2   string `json:"address2,omitempty"`
+	City       string `json:"city"`
+	State      string `json:"state"`
+	PostalCode string `json:"postalCode"`
+	Country    string `json:"country"`
+}
+
+func (req customerRequest) validate() error {
+	if req.FirstName == "" || req.LastName == "" {
+		return errors.New("create customer: empty name field(s)")
+	}
+	if req.Email == "" {
+		return errors.New("create customer: empty email")
+	}
+	if len(req.Phones) == 0 {
+		return errors.New("create customer: phone array is required")
+	}
+	if len(req.Addresses) == 0 {
+		return errors.New("create customer: address array is required")
+	}
+	return nil
+}
+
+func (req customerRequest) asCustomer() client.Customer {
+	// TODO(adam): How do we store off SSN (and wipe from models)
+	customer := client.Customer{
+		Id:         base.ID(),
+		FirstName:  req.FirstName,
+		MiddleName: req.MiddleName,
+		LastName:   req.LastName,
+		NickName:   req.NickName,
+		Suffix:     req.Suffix,
+		BirthDate:  req.BirthDate,
+		Email:      req.Email,
+		Status:     "Applied",
+	}
+	for i := range req.Phones {
+		customer.Phones = append(customer.Phones, client.Phone{
+			Number: req.Phones[i].Number,
+			Type:   req.Phones[i].Type,
+		})
+	}
+	for i := range req.Addresses {
+		customer.Addresses = append(customer.Addresses, client.Address{
+			Address1:   req.Addresses[i].Address1,
+			Address2:   req.Addresses[i].Address2,
+			City:       req.Addresses[i].City,
+			State:      req.Addresses[i].State,
+			PostalCode: req.Addresses[i].PostalCode,
+			Country:    req.Addresses[i].Country,
+			Active:     true,
+		})
+	}
+	return customer
+}
+
+func createCustomer(logger log.Logger, repo customerRepository) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		w = wrapResponseWriter(logger, w, r)
+
+		var req customerRequest
+		if err := json.NewDecoder(r.Body).Decode(&req); err != nil {
+			moovhttp.Problem(w, err)
+			return
+		}
+		if err := req.validate(); err != nil {
+			moovhttp.Problem(w, err)
+			return
+		}
+
+		cust, err := repo.createCustomer(req)
+		if err != nil {
+			if requestId := moovhttp.GetRequestId(r); requestId != "" {
+				logger.Log("accounts", fmt.Sprintf("%v", err), "requestId", requestId)
+			}
+			moovhttp.Problem(w, err)
+			return
+		}
+
+		w.Header().Set("Content-Type", "application/json; charset=utf-8")
+		w.WriteHeader(http.StatusOK)
+		json.NewEncoder(w).Encode(cust)
+	}
+}
+
+type customerRepository interface {
+	createCustomer(req customerRequest) (*client.Customer, error)
+	getCustomer(customerId string) (*client.Customer, error)
+}
+
+type sqliteCustomerRepository struct {
+	db *sql.DB
+}
+
+func (r *sqliteCustomerRepository) close() error {
+	return r.db.Close()
+}
+
+func (r *sqliteCustomerRepository) createCustomer(req customerRequest) (*client.Customer, error) {
+	c := req.asCustomer()
+
+	tx, err := r.db.Begin()
+	if err != nil {
+		return nil, err
+	}
+
+	// Insert customer record
+	query := `insert into customers (customer_id, first_name, middle_name, last_name, nick_name, suffix, birthdate, status, email, created_at, last_modified)
+values (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?);`
+	stmt, err := tx.Prepare(query)
+	if err != nil {
+		return nil, err
+	}
+	now := time.Now()
+	_, err = stmt.Exec(c.Id, c.FirstName, c.MiddleName, c.LastName, c.NickName, c.Suffix, c.BirthDate, c.Status, c.Email, now, now)
+	if err != nil {
+		return nil, fmt.Errorf("createCustomer: insert into customers err=%v | rollback=%v", err, tx.Rollback())
+	}
+	stmt.Close()
+
+	// Insert customer phone numbers
+	query = `insert or replace into customers_phones (customer_id, number, valid, type) values (?, ?, ?, ?);`
+	stmt, err = tx.Prepare(query)
+	if err != nil {
+		return nil, fmt.Errorf("createCustomer: insert into customers_phones err=%v | rollback=%v", err, tx.Rollback())
+	}
+	for i := range c.Phones {
+		_, err := stmt.Exec(c.Id, c.Phones[i].Number, c.Phones[i].Valid, c.Phones[i].Type)
+		if err != nil {
+			stmt.Close()
+			return nil, fmt.Errorf("createCustomer: customers_phones exec err=%v | rollback=%v", err, tx.Rollback())
+		}
+	}
+	stmt.Close()
+
+	// Insert customer addresses
+	query = `insert or replace into customers_addresses(customer_id, type, address1, address2, city, state, postal_code, country, validated, active) values (?, ?, ?, ?, ?, ?, ?, ?, ?, ?);`
+	stmt, err = tx.Prepare(query)
+	if err != nil {
+		return nil, fmt.Errorf("createCustomer: insert into customers_addresses err=%v | rollback=%v", err, tx.Rollback())
+	}
+	for i := range c.Addresses {
+		_, err := stmt.Exec(c.Id, c.Addresses[i].Type, c.Addresses[i].Address1, c.Addresses[i].Address2, c.Addresses[i].City, c.Addresses[i].State, c.Addresses[i].PostalCode, c.Addresses[i].Country, c.Addresses[i].Validated, c.Addresses[i].Active)
+		if err != nil {
+			stmt.Close()
+			return nil, fmt.Errorf("createCustomer: customers_addresses exec err=%v | rollback=%v", err, tx.Rollback())
+		}
+	}
+	stmt.Close()
+
+	if err := tx.Commit(); err != nil {
+		return nil, fmt.Errorf("createCustomer: tx.Commit: %v", err)
+	}
+	return &c, nil
+}
+
+func (r *sqliteCustomerRepository) getCustomer(customerId string) (*client.Customer, error) {
+	// TODO(adam): read all DB fields once we handle all in the request
+	query := `select first_name, middle_name, last_name, nick_name, suffix, birthdate, status, email, created_at, last_modified from customers where customer_id = ? and deleted_at is null limit 1;`
+	stmt, err := r.db.Prepare(query)
+	if err != nil {
+		return nil, err
+	}
+
+	row := stmt.QueryRow(customerId)
+
+	var cust client.Customer
+	cust.Id = customerId
+	err = row.Scan(&cust.FirstName, &cust.MiddleName, &cust.LastName, &cust.NickName, &cust.Suffix, &cust.BirthDate, &cust.Status, &cust.Email, &cust.CreatedAt, &cust.LastModified)
+	stmt.Close()
+	if err != nil && !strings.Contains(err.Error(), "no rows in result set") {
+		return nil, fmt.Errorf("getCustomer: %v", err)
+	}
+	if cust.FirstName == "" {
+		return nil, nil // not found
+	}
+
+	phones, err := r.readPhones(customerId)
+	if err != nil {
+		return nil, fmt.Errorf("getCustomer: readPhones: %v", err)
+	}
+	cust.Phones = phones
+
+	addresses, err := r.readAddresses(customerId)
+	if err != nil {
+		return nil, fmt.Errorf("getCustomer: readAddresses: %v", err)
+	}
+	cust.Addresses = addresses
+
+	return &cust, nil
+}
+
+func (r *sqliteCustomerRepository) readPhones(customerId string) ([]client.Phone, error) {
+	query := `select number, valid, type from customers_phones where customer_id = ?;`
+	stmt, err := r.db.Prepare(query)
+	if err != nil {
+		return nil, fmt.Errorf("getCustomer: prepare customers_phones: err=%v", err)
+	}
+	defer stmt.Close()
+
+	rows, err := stmt.Query(customerId)
+	if err != nil {
+		return nil, fmt.Errorf("getCustomer: query customers_phones: err=%v", err)
+	}
+	defer rows.Close()
+
+	var phones []client.Phone
+	for rows.Next() {
+		var p client.Phone
+		if err := rows.Scan(&p.Number, &p.Valid, &p.Type); err != nil {
+			return nil, fmt.Errorf("getCustomer: scan customers_phones: err=%v", err)
+		}
+		phones = append(phones, p)
+	}
+	return phones, rows.Err()
+}
+
+func (r *sqliteCustomerRepository) readAddresses(customerId string) ([]client.Address, error) {
+	query := `select type, address1, address2, city, state, postal_code, country, validated, active from customers_addresses where customer_id = ?;`
+	stmt, err := r.db.Prepare(query)
+	if err != nil {
+		return nil, fmt.Errorf("readAddresses: prepare customers_addresses: err=%v", err)
+	}
+	defer stmt.Close()
+
+	rows, err := stmt.Query(customerId)
+	if err != nil {
+		return nil, fmt.Errorf("readAddresses: query customers_addresses: err=%v", err)
+	}
+	defer rows.Close()
+
+	var adds []client.Address
+	for rows.Next() {
+		var a client.Address
+		if err := rows.Scan(&a.Type, &a.Address1, &a.Address2, &a.City, &a.State, &a.PostalCode, &a.Country, &a.Validated, &a.Active); err != nil {
+			return nil, fmt.Errorf("readAddresses: scan customers_addresses: err=%v", err)
+		}
+		adds = append(adds, a)
+	}
+	return adds, rows.Err()
+}
---
+++ cmd/server/customers_test.go
@@ -0,0 +1,207 @@
+// Copyright 2018 The Moov Authors
+// Use of this source code is governed by an Apache License
+// license that can be found in the LICENSE file.
+
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"net/http"
+	"net/http/httptest"
+	"strings"
+	"testing"
+
+	"github.com/moov-io/base"
+	client "github.com/moov-io/customers/client"
+
+	"github.com/gorilla/mux"
+)
+
+func TestCustomers__GetCustomer(t *testing.T) {
+	repo := createTestCustomerRepository(t)
+	defer repo.close()
+	cust, err := repo.createCustomer(customerRequest{
+		FirstName: "Jane",
+		LastName:  "Doe",
+		Email:     "jane@example.com",
+	})
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	w := httptest.NewRecorder()
+	req := httptest.NewRequest("GET", fmt.Sprintf("/customers/%s", cust.Id), nil)
+	req.Header.Set("x-user-id", "test")
+
+	router := mux.NewRouter()
+	addCustomerRoutes(nil, router, repo)
+	router.ServeHTTP(w, req)
+	w.Flush()
+
+	if w.Code != http.StatusOK {
+		t.Errorf("bogus status code: %d", w.Code)
+	}
+
+	var customer client.Customer // TODO(adam): check more of Customer response?
+	if err := json.NewDecoder(w.Body).Decode(&customer); err != nil {
+		t.Fatal(err)
+	}
+	if customer.Id == "" {
+		t.Error("empty Customer.Id")
+	}
+}
+
+func TestCustomers__customerRequest(t *testing.T) {
+	req := &customerRequest{}
+	if err := req.validate(); err == nil {
+		t.Error("expected error")
+	}
+	req.FirstName = "jane"
+	req.LastName = "doe"
+	if err := req.validate(); err == nil {
+		t.Error("expected error")
+	}
+	req.Email = "jane.doe@example.com"
+	if err := req.validate(); err == nil {
+		t.Error("expected error")
+	}
+	req.Phones = append(req.Phones, phone{
+		Number: "123.456.7890",
+		Type:   "Checking",
+	})
+	if err := req.validate(); err == nil {
+		t.Error("expected error")
+	}
+	req.Addresses = append(req.Addresses, address{
+		Address1:   "123 1st st",
+		City:       "fake city",
+		State:      "CA",
+		PostalCode: "90210",
+		Country:    "US",
+	})
+	if err := req.validate(); err != nil {
+		t.Errorf("unexpected error: %v", err)
+	}
+
+	// asCustomer
+	cust := req.asCustomer()
+	if cust.Id == "" {
+		t.Errorf("empty Customer: %#v", cust)
+	}
+	if len(cust.Phones) != 1 {
+		t.Errorf("cust.Phones: %#v", cust.Phones)
+	}
+	if len(cust.Addresses) != 1 {
+		t.Errorf("cust.Addresses: %#v", cust.Addresses)
+	}
+}
+
+func TestCustomers__createCustomer(t *testing.T) {
+	w := httptest.NewRecorder()
+	phone := `{"number": "555.555.5555", "type": "mobile"}`
+	address := `{"type": "home", "address1": "123 1st St", "city": "Denver", "state": "CO", "postalCode": "12345", "country": "USA"}`
+	body := fmt.Sprintf(`{"firstName": "jane", "lastName": "doe", "email": "jane@example.com", "phones": [%s], "addresses": [%s]}`, phone, address)
+	req := httptest.NewRequest("POST", "/customers", strings.NewReader(body))
+	req.Header.Set("x-user-id", "test")
+
+	repo := createTestCustomerRepository(t)
+	defer repo.close()
+
+	router := mux.NewRouter()
+	addCustomerRoutes(nil, router, repo)
+	router.ServeHTTP(w, req)
+	w.Flush()
+
+	if w.Code != http.StatusOK {
+		t.Errorf("bogus status code: %d", w.Code)
+	}
+
+	var cust client.Customer // TODO(adam): check more of Customer response?
+	if err := json.NewDecoder(w.Body).Decode(&cust); err != nil {
+		t.Fatal(err)
+	}
+	if cust.Id == "" {
+		t.Error("empty Customer.Id")
+	}
+
+	// sad path
+	w = httptest.NewRecorder()
+	req = httptest.NewRequest("POST", "/customers", strings.NewReader("null"))
+	req.Header.Set("x-user-id", "test")
+	router.ServeHTTP(w, req)
+	w.Flush()
+
+	if w.Code != http.StatusBadRequest {
+		t.Fatalf("bogus HTTP status code: %d", w.Code)
+	}
+}
+
+func createTestCustomerRepository(t *testing.T) *sqliteCustomerRepository {
+	t.Helper()
+	db, err := createTestSqliteDB()
+	if err != nil {
+		t.Fatal(err)
+	}
+	if err := migrate(nil, db.db); err != nil {
+		t.Fatal(err)
+	}
+	return &sqliteCustomerRepository{db.db}
+}
+
+func TestCustomers__repository(t *testing.T) {
+	repo := createTestCustomerRepository(t)
+	defer repo.close()
+
+	cust, err := repo.getCustomer(base.ID())
+	if err != nil {
+		t.Errorf("unexpected error: %v", err)
+	}
+	if cust != nil {
+		t.Error("expected no Customer")
+	}
+
+	// write
+	req := customerRequest{
+		FirstName: "Jane",
+		LastName:  "Doe",
+		Email:     "jane@example.com",
+		Phones: []phone{
+			{
+				Number: "123.456.7890",
+				Type:   "Checking",
+			},
+		},
+		Addresses: []address{
+			{
+				Address1:   "123 1st st",
+				City:       "fake city",
+				State:      "CA",
+				PostalCode: "90210",
+				Country:    "US",
+			},
+		},
+	}
+	cust, err = repo.createCustomer(req)
+	if err != nil {
+		t.Error(err)
+	}
+	if cust == nil {
+		t.Fatal("nil Customer")
+	}
+	if len(cust.Phones) != 1 || len(cust.Addresses) != 1 {
+		t.Errorf("len(cust.Phones)=%d and len(cust.Addresses)=%d", len(cust.Phones), len(cust.Addresses))
+	}
+
+	// read
+	cust, err = repo.getCustomer(cust.Id)
+	if err != nil {
+		t.Error(err)
+	}
+	if cust == nil {
+		t.Fatal("nil Customer")
+	}
+	if len(cust.Phones) != 1 || len(cust.Addresses) != 1 {
+		t.Errorf("len(cust.Phones)=%d and len(cust.Addresses)=%d", len(cust.Phones), len(cust.Addresses))
+	}
+}
