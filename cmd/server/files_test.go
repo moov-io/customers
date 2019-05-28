d24fc1d58bffa80b4b8cb1115f025e820b3f0104
Adam Shannon <adamkshannon@gmail.com>
2019-05-28 18:14:32 -0500

    cmd/server: test local file proxy route

--- cmd/server/files_test.go
+++ cmd/server/files_test.go
@@ -5,9 +5,83 @@
 package main
 
 import (
+	"context"
+	"io"
+	"net/http"
+	"net/http/httptest"
+	"net/url"
+	"os"
+	"path/filepath"
 	"testing"
+	"time"
+
+	"github.com/moov-io/base"
+
+	"github.com/gorilla/mux"
+	"gocloud.dev/blob"
 )
 
 func TestFiles__proxyLocalFile(t *testing.T) {
+	// first, upload a file
+	bucket, err := testBucket()
+	if err != nil {
+		t.Fatal(err)
+	}
 
+	customerId, documentId := base.ID(), base.ID()
+	documentKey := makeDocumentKey(customerId, documentId)
+
+	ctx, cancelFn := context.WithTimeout(context.TODO(), 10*time.Second)
+	defer cancelFn()
+
+	writer, err := bucket.NewWriter(ctx, documentKey, &blob.WriterOptions{
+		ContentDisposition: "inline",
+		ContentType:        "image/jpg",
+	})
+	if err != nil {
+		t.Fatal(err)
+	}
+	fd, err := os.Open(filepath.Join("..", "..", "testdata", "colorado.jpg"))
+	if err != nil {
+		t.Fatal(err)
+	}
+	if n, err := io.Copy(writer, fd); err != nil || n == 0 {
+		t.Fatalf("n=%d error=%v", n, err)
+	}
+	writer.Close()
+
+	// Grab our SignedURL
+	signedURL, err := bucket.SignedURL(ctx, documentKey, &blob.SignedURLOptions{
+		Expiry: 15 * time.Minute,
+	})
+	if err != nil {
+		t.Fatal(err)
+	}
+	u, err := url.Parse(signedURL)
+	if u == nil || err != nil {
+		t.Fatalf("u=%s err=%v", u, err)
+	}
+	u.Scheme, u.Host = "", "" // blank out 'http://' and 'localhost' in below assumption
+	signer, err := fileblobSigner("http://localhost", "secret")
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	// Make our request
+	w := httptest.NewRecorder()
+	req := httptest.NewRequest("GET", "/files"+u.String(), nil)
+	router := mux.NewRouter()
+	addFileblobRoutes(nil, router, signer, func() (*blob.Bucket, error) { return bucket, nil })
+	router.ServeHTTP(w, req)
+	w.Flush()
+
+	if w.Code != http.StatusOK {
+		t.Errorf("bogus HTTP status: %d", w.Code)
+	}
+	if v := w.Header().Get("Content-Length"); v != "212203" {
+		t.Errorf("Content-Length: %s", v)
+	}
+	if v := w.Header().Get("Content-Type"); v != "image/jpg" {
+		t.Errorf("Content-Type: %s", v)
+	}
 }
