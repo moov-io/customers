25edc47b3f612a68ff9ea50ebea4a92621510dec
Adam Shannon <adamkshannon@gmail.com>
2020-07-22 18:59:05 -0500

    database/mysql: change customers.status to string

--- internal/database/mysql.go
+++ internal/database/mysql.go
@@ -79,6 +79,10 @@ var (
 			"create_accounts",
 			`create table if not exists accounts(account_id varchar(40) primary key, customer_id varchar(40), user_id varchar(40), encrypted_account_number varchar(40), hashed_account_number varchar(40), masked_account_number varchar(15), routing_number varchar(10), status varchar(12), type varchar(12), created_at datetime, deleted_at datetime);`,
 		),
+		execsql(
+			"alter_customers_status",
+			`alter table customers modify status varchar(20);`,
+		),
 	)
 )
 
