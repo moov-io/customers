620d75edca284810f8daa0a13a00e58b24965cbd
Pavel Gabriel <alovak@gmail.com>
2020-08-28 21:25:09 +0300

    handle init account validation request

--- cmd/server/accounts/validate_test.go
+++ cmd/server/accounts/validate_test.go
@@ -12,11 +12,16 @@ import (
 	"net/http/httptest"
 	"testing"
 
+	"github.com/google/go-cmp/cmp"
 	"github.com/moov-io/base"
+	"github.com/moov-io/customers/client"
+	"github.com/moov-io/customers/cmd/server/accounts/validator"
+	"github.com/moov-io/customers/cmd/server/accounts/validator/microdeposits"
 	"github.com/moov-io/customers/cmd/server/paygate"
 	"github.com/moov-io/customers/pkg/client"
 	"github.com/moov-io/customers/pkg/secrets"
 	payclient "github.com/moov-io/paygate/pkg/client"
+	"github.com/stretchr/testify/require"
 
 	"github.com/go-kit/kit/log"
 	"github.com/gorilla/mux"
@@ -64,4 +69,197 @@ func TestRouter__ValidateAccounts(t *testing.T) {
 	if w.Code != http.StatusOK {
 		t.Errorf("bogus HTTP status: %d", w.Code)
 	}
+}
+
+type accountValidationRequest struct {
+	Strategy string `json:"strategy"`
+	Vendor   string `json:"vendor"`
+}
+
+func TestRouter__InitAccountValidation(t *testing.T) {
+	customerID, userID := base.ID(), base.ID()
+	repo := setupTestAccountRepository(t)
+	// keeper := secrets.TestStringKeeper(t)
+
+	strategies := map[validator.StrategyKey]validator.Strategy{
+		validator.StrategyKey{"test", "moov"}: validator.TestStrategy(),
+	}
+
+	// create account
+	acc, err := repo.createCustomerAccount(customerID, userID, &createAccountRequest{
+		AccountNumber: "123",
+		RoutingNumber: "987654320",
+		Type:          client.CHECKING,
+	})
+	require.NoError(t, err)
+
+	t.Run("Test micro-deposits strategy", func(t *testing.T) {
+		paygateClient := &paygate.MockClient{
+			// this is how we make call to initiate micro deposit successful
+			Err: nil,
+		}
+
+		strategies := map[validator.StrategyKey]validator.Strategy{
+			validator.StrategyKey{"micro-deposits", "moov"}: microdeposits.NewStrategy(paygateClient),
+		}
+
+		// build request with strategy params
+		params := map[string]string{
+			"strategy": "micro-deposits",
+		}
+
+		var buf bytes.Buffer
+		err := json.NewEncoder(&buf).Encode(params)
+		require.NoError(t, err)
+		body := bytes.NewReader(buf.Bytes())
+
+		w := httptest.NewRecorder()
+		req, err := http.NewRequest("POST", "/", body)
+		require.NoError(t, err)
+		req.Header.Set("Content-Type", "application/json")
+		req = mux.SetURLVars(req, map[string]string{"customerID": customerID, "accountID": acc.AccountID})
+
+		handler := initAccountValidation(log.NewNopLogger(), repo, strategies)
+		handler(w, req)
+
+		require.Equal(t, http.StatusOK, w.Code)
+
+		// // create account
+		// acct, err := repo.createCustomerAccount(customerID, userID, &createAccountRequest{
+		// 	AccountNumber: "123",
+		// 	RoutingNumber: "987654320",
+		// 	Type:          client.CHECKING,
+		// })
+		// if err != nil {
+		// 	t.Fatal(err)
+		// }
+
+		// // make our validation request
+		// var body bytes.Buffer
+		// if err := json.NewEncoder(&body).Encode(client.UpdateValidation{
+		// 	Strategy:      "micro-deposits",
+		// 	MicroDeposits: []string{"USD 0.03", "USD 0.07"},
+		// }); err != nil {
+		// 	t.Fatal(err)
+		// }
+		// req := httptest.NewRequest("PUT", fmt.Sprintf("/customers/%s/accounts/%s/validate", customerID, acct.AccountID), &body)
+
+		// w := httptest.NewRecorder()
+		// handler.ServeHTTP(w, req)
+		// w.Flush()
+
+		// if w.Code != http.StatusOK {
+		// 	t.Errorf("bogus HTTP status: %d", w.Code)
+		// }
+	})
+
+	t.Run("Test unknown strategy is requested", func(t *testing.T) {
+		params := map[string]string{
+			"strategy": "unknown",
+		}
+
+		var buf bytes.Buffer
+		err := json.NewEncoder(&buf).Encode(params)
+		require.NoError(t, err)
+		body := bytes.NewReader(buf.Bytes())
+
+		w := httptest.NewRecorder()
+		req, err := http.NewRequest("POST", "/", body)
+		require.NoError(t, err)
+		req.Header.Set("Content-Type", "application/json")
+		req = mux.SetURLVars(req, map[string]string{"customerID": customerID, "accountID": acc.AccountID})
+
+		handler := initAccountValidation(log.NewNopLogger(), repo, strategies)
+		handler(w, req)
+
+		require.Equal(t, http.StatusBadRequest, w.Code)
+		require.Contains(t, w.Body.String(), "strategy unknown for vendor moov was not found")
+	})
+
+	t.Run("Test 'test' strategy", func(t *testing.T) {
+		params := map[string]string{
+			"strategy": "test",
+		}
+
+		var buf bytes.Buffer
+		err := json.NewEncoder(&buf).Encode(params)
+		require.NoError(t, err)
+		body := bytes.NewReader(buf.Bytes())
+
+		w := httptest.NewRecorder()
+		req, err := http.NewRequest("POST", "/", body)
+		require.NoError(t, err)
+		req.Header.Set("Content-Type", "application/json")
+		req = mux.SetURLVars(req, map[string]string{"customerID": customerID, "accountID": acc.AccountID})
+
+		handler := initAccountValidation(log.NewNopLogger(), repo, strategies)
+		handler(w, req)
+
+		require.Equal(t, http.StatusOK, w.Code)
+
+		type validationResponse struct {
+			ValidationID   string            `json:"validationID"`
+			Status         string            `json:"status"`
+			Strategy       string            `json:"strategy"`
+			Vendor         string            `json:"vendor"`
+			VendorResponse map[string]string `json:"vendor_response"`
+		}
+
+		want := &validationResponse{
+			ValidationID: "1234",
+			Status:       "pending",
+			Strategy:     "test",
+			Vendor:       "moov",
+			VendorResponse: map[string]string{
+				"test": "ok",
+			},
+		}
+
+		got := &validationResponse{}
+		json.NewDecoder(w.Body).Decode(got)
+		if diff := cmp.Diff(got, want); len(diff) != 0 {
+			t.Errorf(diff)
+		}
+	})
+
+	// fmt.Println(strategies)
+
+	// require.NoError(t, err)
+	// fmt.Println(acc)
+
+	// handler := initAccountValidation(log.NewNopLogger(), repo, strategies)
+
+	// w := httptest.NewRecorder()
+	// req, err := http.NewRequest("POST", "/", nil)
+	// require.NoError(t, err)
+
+	// // mux.SetURLVars(req, map[string]string{"foo": "bar"})
+	// handler(w, req)
+
+	// type validationResponse struct {
+	// 	ValidationID   string            `json:"validationID"`
+	// 	Status         string            `json:"status"`
+	// 	Strategy       string            `json:"strategy"`
+	// 	Vendor         string            `json:"vendor"`
+	// 	VendorResponse map[string]string `json:"vendor_response"`
+	// }
+
+	// want := &validationResponse{
+	// 	ValidationID: "1234",
+	// 	Status:       "pending",
+	// 	Strategy:     "test",
+	// 	Vendor:       "moov",
+	// 	VendorResponse: map[string]string{
+	// 		"test": "ok",
+	// 	},
+	// }
+
+	// fmt.Println(w.Body.String())
+
+	// got := &validationResponse{}
+	// json.NewDecoder(w.Body).Decode(got)
+	// if diff := cmp.Diff(got, want); len(diff) != 0 {
+	// 	t.Errorf(diff)
+	// }
+
 }
