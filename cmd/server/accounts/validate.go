fae56cc256ca178a4e3c65be606977d00ea4d9c5
Pavel Gabriel <alovak@gmail.com>
2020-08-30 18:20:46 +0300

    add completeAccountValidation handler

--- cmd/server/accounts/validate.go
+++ cmd/server/accounts/validate.go
@@ -8,13 +8,11 @@ import (
 	"encoding/json"
 	"fmt"
 	"net/http"
-	"strings"
 
 	"github.com/gorilla/mux"
 	moovhttp "github.com/moov-io/base/http"
-	"github.com/moov-io/customers/client"
+	"github.com/moov-io/customers/admin"
 	"github.com/moov-io/customers/cmd/server/accounts/validator"
-	"github.com/moov-io/customers/cmd/server/paygate"
 	"github.com/moov-io/customers/cmd/server/route"
 	"github.com/moov-io/customers/pkg/client"
 
@@ -28,22 +26,23 @@ func initAccountValidation(logger log.Logger, repo Repository, strategies map[va
 	}
 
 	type response struct {
-		ValidationID   string                    `json:"validationID"`
-		Status         string                    `json:"status"`
-		Strategy       string                    `json:"strategy"`
-		Vendor         string                    `json:"vendor"`
+		// TODO: do we want to create DB records for validation?
+		// following fields may be stored in DB
+		// ValidationID   string                    `json:"validationID"`
+		// Status         string                    `json:"status"`
+		// Strategy       string                    `json:"strategy"`
+		// Vendor         string                    `json:"vendor"`
 		VendorResponse *validator.VendorResponse `json:"vendor_response"`
 	}
 
 	return func(w http.ResponseWriter, r *http.Request) {
-		// check required parameters
 		w = route.Responder(logger, w, r)
 
 		// TODO discuss
 		// following methods Get...ID have side effect inside: moovhttp.Problem(w, ErrNoCustomerID)
 		// customerID, accountID := route.GetCustomerID(w, r), getAccountID(w, r)
 		vars := mux.Vars(r)
-		customerID, accountID := vars["customerID"], vars["accountID"]
+		userID, customerID, accountID := moovhttp.GetUserID(r), vars["customerID"], vars["accountID"]
 
 		if customerID == "" || accountID == "" {
 			moovhttp.Problem(w, fmt.Errorf("missing customerID: %s and/or accountID: %s", customerID, accountID))
@@ -77,11 +76,8 @@ func initAccountValidation(logger log.Logger, repo Repository, strategies map[va
 			return
 		}
 
-		// within transaction create validation
-		// ...
-
 		// execute strategy and get vendor response
-		vendorResponse, err := strategy.InitAccountValidation("", "", "")
+		vendorResponse, err := strategy.InitAccountValidation(userID, accountID, customerID)
 		if err != nil {
 			moovhttp.Problem(w, err)
 			return
@@ -89,10 +85,6 @@ func initAccountValidation(logger log.Logger, repo Repository, strategies map[va
 
 		// render validation with vendor response
 		res := &response{
-			ValidationID:   "1234",
-			Status:         "pending",
-			Strategy:       "test",
-			Vendor:         "moov",
 			VendorResponse: vendorResponse,
 		}
 
@@ -102,45 +94,80 @@ func initAccountValidation(logger log.Logger, repo Repository, strategies map[va
 	}
 }
 
-func validateAccount(logger log.Logger, repo Repository, paygateClient paygate.Client) http.HandlerFunc {
+func completeAccountValidation(logger log.Logger, repo Repository, strategies map[validator.StrategyKey]validator.Strategy) http.HandlerFunc {
+	type request struct {
+		Strategy      string                   `json:"strategy"`
+		Vendor        string                   `json:"vendor"`
+		VendorRequest *validator.VendorRequest `json:"vendor_request"`
+	}
+
+	type response struct {
+		VendorResponse *validator.VendorResponse `json:"vendor_response"`
+	}
+
 	return func(w http.ResponseWriter, r *http.Request) {
 		w = route.Responder(logger, w, r)
 
-		customerID, accountID := route.GetCustomerID(w, r), getAccountID(w, r)
+		vars := mux.Vars(r)
+		userID, customerID, accountID := moovhttp.GetUserID(r), vars["customerID"], vars["accountID"]
+
 		if customerID == "" || accountID == "" {
+			moovhttp.Problem(w, fmt.Errorf("missing customerID: %s and/or accountID: %s", customerID, accountID))
 			return
 		}
 
-		// Lookup the account and verify it needs to be validated
-		account, err := repo.getCustomerAccount(customerID, accountID)
+		// check if account is not validated yet
+		// ...
+
+		// decode request params
+		req := &request{}
+		if err := json.NewDecoder(r.Body).Decode(req); err != nil {
+			moovhttp.Problem(w, fmt.Errorf("unable to read request: %v", err))
+			return
+		}
+
+		// set default vendor if not specified
+		// if we have Validation record we can get strategy and vendor from it
+		// for now let's keep it as is
+		if req.Vendor == "" {
+			req.Vendor = "moov"
+		}
+
+		// find requested strategy
+		strategyKey := validator.StrategyKey{
+			Strategy: req.Strategy,
+			Vendor:   req.Vendor,
+		}
+
+		strategy, found := strategies[strategyKey]
+		if !found {
+			moovhttp.Problem(w, fmt.Errorf("strategy %s for vendor %s was not found", req.Strategy, req.Vendor))
+			return
+		}
+
+		// execute strategy and get vendor response
+		vendorResponse, err := strategy.CompleteAccountValidation(userID, accountID, customerID, req.VendorRequest)
 		if err != nil {
 			moovhttp.Problem(w, err)
 			return
 		}
-		if !strings.EqualFold(string(account.Status), string(client.NONE)) {
-			moovhttp.Problem(w, fmt.Errorf("unexpected accountID=%s status=%s", accountID, account.Status))
+
+		// TODO: why admin.VALIDATED is used here?
+		// account.Status is client.AccountStatus (with client.VALIDATED)
+		// should I change it into client.VALIDATED?
+		err = repo.updateAccountStatus(accountID, admin.VALIDATED)
+		if err != nil {
+			moovhttp.Problem(w, err)
 			return
 		}
 
-		var req client.UpdateValidation
-		if err := json.NewDecoder(r.Body).Decode(&req); err != nil {
-			moovhttp.Problem(w, fmt.Errorf("unable to read UpdateValidation: %v", err))
-			return
+		// render validation with vendor response
+		res := &response{
+			VendorResponse: vendorResponse,
 		}
 
-		switch req.Strategy {
-		case "micro-deposits":
-			userID := moovhttp.GetUserID(r)
-			if err := handleMicroDepositValidation(repo, paygateClient, accountID, customerID, userID, req.MicroDeposits); err != nil {
-				moovhttp.Problem(w, err)
-				return
-			}
-
-		default:
-			moovhttp.Problem(w, fmt.Errorf("unknown strategy %s", req.Strategy))
-			return
-		}
-
+		w.Header().Set("Content-Type", "application/json; charset=utf-8")
 		w.WriteHeader(http.StatusOK)
+		json.NewEncoder(w).Encode(res)
 	}
 }
--- cmd/server/accounts/validate_test.go
+++ cmd/server/accounts/validate_test.go
@@ -7,74 +7,22 @@ package accounts
 import (
 	"bytes"
 	"encoding/json"
-	"fmt"
 	"net/http"
 	"net/http/httptest"
 	"testing"
 
 	"github.com/google/go-cmp/cmp"
 	"github.com/moov-io/base"
-	"github.com/moov-io/customers/client"
 	"github.com/moov-io/customers/cmd/server/accounts/validator"
 	"github.com/moov-io/customers/cmd/server/accounts/validator/microdeposits"
 	"github.com/moov-io/customers/cmd/server/paygate"
 	"github.com/moov-io/customers/pkg/client"
-	"github.com/moov-io/customers/pkg/secrets"
 	payclient "github.com/moov-io/paygate/pkg/client"
 	"github.com/stretchr/testify/require"
 
 	"github.com/go-kit/kit/log"
 	"github.com/gorilla/mux"
 )
-
-func TestRouter__ValidateAccounts(t *testing.T) {
-	customerID, userID := base.ID(), base.ID()
-	repo := setupTestAccountRepository(t)
-	keeper := secrets.TestStringKeeper(t)
-
-	paygateClient := &paygate.MockClient{
-		Micro: &payclient.MicroDeposits{
-			Amounts: []string{"USD 0.03", "USD 0.07"},
-			Status:  payclient.PROCESSED,
-		},
-	}
-
-	handler := mux.NewRouter()
-	RegisterRoutes(log.NewNopLogger(), handler, repo, testFedClient, paygateClient, keeper, keeper)
-
-	// create account
-	acct, err := repo.createCustomerAccount(customerID, userID, &createAccountRequest{
-		AccountNumber: "123",
-		RoutingNumber: "987654320",
-		Type:          client.CHECKING,
-	})
-	if err != nil {
-		t.Fatal(err)
-	}
-
-	// make our validation request
-	var body bytes.Buffer
-	if err := json.NewEncoder(&body).Encode(client.UpdateValidation{
-		Strategy:      "micro-deposits",
-		MicroDeposits: []string{"USD 0.03", "USD 0.07"},
-	}); err != nil {
-		t.Fatal(err)
-	}
-	req := httptest.NewRequest("PUT", fmt.Sprintf("/customers/%s/accounts/%s/validate", customerID, acct.AccountID), &body)
-
-	w := httptest.NewRecorder()
-	handler.ServeHTTP(w, req)
-	w.Flush()
-
-	if w.Code != http.StatusOK {
-		t.Errorf("bogus HTTP status: %d", w.Code)
-	}
-}
-
-type accountValidationRequest struct {
-	Strategy string `json:"strategy"`
-	Vendor   string `json:"vendor"`
-}
 
 func TestRouter__InitAccountValidation(t *testing.T) {
 	customerID, userID := base.ID(), base.ID()
@@ -92,6 +40,9 @@ func TestRouter__InitAccountValidation(t *testing.T) {
 		Type:          client.CHECKING,
 	})
 	require.NoError(t, err)
+
+	t.Run("Test when account is validated already", func(t *testing.T) {
+	})
 
 	t.Run("Test micro-deposits strategy", func(t *testing.T) {
 		paygateClient := &paygate.MockClient{
@@ -123,34 +74,6 @@ func TestRouter__InitAccountValidation(t *testing.T) {
 		handler(w, req)
 
 		require.Equal(t, http.StatusOK, w.Code)
-
-		// // create account
-		// acct, err := repo.createCustomerAccount(customerID, userID, &createAccountRequest{
-		// 	AccountNumber: "123",
-		// 	RoutingNumber: "987654320",
-		// 	Type:          client.CHECKING,
-		// })
-		// if err != nil {
-		// 	t.Fatal(err)
-		// }
-
-		// // make our validation request
-		// var body bytes.Buffer
-		// if err := json.NewEncoder(&body).Encode(client.UpdateValidation{
-		// 	Strategy:      "micro-deposits",
-		// 	MicroDeposits: []string{"USD 0.03", "USD 0.07"},
-		// }); err != nil {
-		// 	t.Fatal(err)
-		// }
-		// req := httptest.NewRequest("PUT", fmt.Sprintf("/customers/%s/accounts/%s/validate", customerID, acct.AccountID), &body)
-
-		// w := httptest.NewRecorder()
-		// handler.ServeHTTP(w, req)
-		// w.Flush()
-
-		// if w.Code != http.StatusOK {
-		// 	t.Errorf("bogus HTTP status: %d", w.Code)
-		// }
 	})
 
 	t.Run("Test unknown strategy is requested", func(t *testing.T) {
@@ -198,18 +121,10 @@ func TestRouter__InitAccountValidation(t *testing.T) {
 		require.Equal(t, http.StatusOK, w.Code)
 
 		type validationResponse struct {
-			ValidationID   string            `json:"validationID"`
-			Status         string            `json:"status"`
-			Strategy       string            `json:"strategy"`
-			Vendor         string            `json:"vendor"`
 			VendorResponse map[string]string `json:"vendor_response"`
 		}
 
 		want := &validationResponse{
-			ValidationID: "1234",
-			Status:       "pending",
-			Strategy:     "test",
-			Vendor:       "moov",
 			VendorResponse: map[string]string{
 				"test": "ok",
 			},
@@ -221,45 +136,59 @@ func TestRouter__InitAccountValidation(t *testing.T) {
 			t.Errorf(diff)
 		}
 	})
+}
 
-	// fmt.Println(strategies)
+func TestRouter__CompleteAccountValidation(t *testing.T) {
+	customerID, userID := base.ID(), base.ID()
+	repo := setupTestAccountRepository(t)
 
-	// require.NoError(t, err)
-	// fmt.Println(acc)
+	// create account
+	acc, err := repo.createCustomerAccount(customerID, userID, &createAccountRequest{
+		AccountNumber: "123",
+		RoutingNumber: "987654320",
+		Type:          client.CHECKING,
+	})
+	require.NoError(t, err)
 
-	// handler := initAccountValidation(log.NewNopLogger(), repo, strategies)
+	t.Run("Test CompleteAccountValidation with micro-deposits strategy", func(t *testing.T) {
+		paygateClient := &paygate.MockClient{
+			Micro: &payclient.MicroDeposits{
+				MicroDepositID: base.ID(),
+				Amounts:        []string{"USD 0.03", "USD 0.07"},
+				Status:         payclient.PROCESSED,
+			},
+		}
 
-	// w := httptest.NewRecorder()
-	// req, err := http.NewRequest("POST", "/", nil)
-	// require.NoError(t, err)
+		strategies := map[validator.StrategyKey]validator.Strategy{
+			validator.StrategyKey{"micro-deposits", "moov"}: microdeposits.NewStrategy(paygateClient),
+		}
 
-	// // mux.SetURLVars(req, map[string]string{"foo": "bar"})
-	// handler(w, req)
+		// build request with strategy params
+		params := map[string]interface{}{
+			"strategy": "micro-deposits",
+			"vendor_request": map[string][]string{
+				"micro-deposits": []string{"USD 0.03", "USD 0.07"},
+			},
+		}
 
-	// type validationResponse struct {
-	// 	ValidationID   string            `json:"validationID"`
-	// 	Status         string            `json:"status"`
-	// 	Strategy       string            `json:"strategy"`
-	// 	Vendor         string            `json:"vendor"`
-	// 	VendorResponse map[string]string `json:"vendor_response"`
-	// }
+		var buf bytes.Buffer
+		err := json.NewEncoder(&buf).Encode(params)
+		require.NoError(t, err)
+		body := bytes.NewReader(buf.Bytes())
 
-	// want := &validationResponse{
-	// 	ValidationID: "1234",
-	// 	Status:       "pending",
-	// 	Strategy:     "test",
-	// 	Vendor:       "moov",
-	// 	VendorResponse: map[string]string{
-	// 		"test": "ok",
-	// 	},
-	// }
+		w := httptest.NewRecorder()
+		req, err := http.NewRequest("PUT", "/", body)
+		require.NoError(t, err)
+		req.Header.Set("Content-Type", "application/json")
+		req = mux.SetURLVars(req, map[string]string{"customerID": customerID, "accountID": acc.AccountID})
 
-	// fmt.Println(w.Body.String())
+		handler := completeAccountValidation(log.NewNopLogger(), repo, strategies)
+		handler(w, req)
 
-	// got := &validationResponse{}
-	// json.NewDecoder(w.Body).Decode(got)
-	// if diff := cmp.Diff(got, want); len(diff) != 0 {
-	// 	t.Errorf(diff)
-	// }
+		require.Equal(t, http.StatusOK, w.Code)
 
+		updatedAccount, err := repo.getCustomerAccount(customerID, acc.AccountID)
+		require.NoError(t, err)
+		require.Equal(t, client.VALIDATED, updatedAccount.Status)
+	})
 }
