ec32d00f611bef7fed25ee537bf647474ee70c40
Pavel Gabriel <alovak@gmail.com>
2020-08-31 11:55:42 +0300

    update tests for test strategy

--- cmd/server/accounts/validate_test.go
+++ cmd/server/accounts/validate_test.go
@@ -18,6 +18,7 @@ import (
 	"github.com/moov-io/customers/client"
 	"github.com/moov-io/customers/cmd/server/accounts/validator"
 	"github.com/moov-io/customers/cmd/server/accounts/validator/microdeposits"
+	"github.com/moov-io/customers/cmd/server/accounts/validator/testvalidator"
 	"github.com/moov-io/customers/cmd/server/paygate"
 	"github.com/moov-io/customers/pkg/client"
 	payclient "github.com/moov-io/paygate/pkg/client"
@@ -33,7 +34,7 @@ func TestRouter__InitAccountValidation(t *testing.T) {
 	// keeper := secrets.TestStringKeeper(t)
 
 	strategies := map[validator.StrategyKey]validator.Strategy{
-		validator.StrategyKey{"test", "moov"}: validator.TestStrategy(),
+		validator.StrategyKey{"test", "moov"}: testvalidator.NewStrategy(),
 	}
 
 	// create account
@@ -75,35 +76,6 @@ func TestRouter__InitAccountValidation(t *testing.T) {
 
 		require.Equal(t, http.StatusBadRequest, w.Code)
 		require.Contains(t, w.Body.String(), fmt.Sprintf("expected accountID=%s status to be 'none'", acc.AccountID))
-	})
-
-	t.Run("Test micro-deposits strategy", func(t *testing.T) {
-		paygateClient := &paygate.MockClient{}
-
-		strategies := map[validator.StrategyKey]validator.Strategy{
-			validator.StrategyKey{"micro-deposits", "moov"}: microdeposits.NewStrategy(paygateClient),
-		}
-
-		// build request with strategy params
-		params := map[string]string{
-			"strategy": "micro-deposits",
-		}
-
-		var buf bytes.Buffer
-		err := json.NewEncoder(&buf).Encode(params)
-		require.NoError(t, err)
-		body := bytes.NewReader(buf.Bytes())
-
-		w := httptest.NewRecorder()
-		req, err := http.NewRequest("POST", "/", body)
-		require.NoError(t, err)
-		req.Header.Set("Content-Type", "application/json")
-		req = mux.SetURLVars(req, map[string]string{"customerID": customerID, "accountID": acc.AccountID})
-
-		handler := initAccountValidation(log.NewNopLogger(), repo, strategies)
-		handler(w, req)
-
-		require.Equal(t, http.StatusOK, w.Code)
 	})
 
 	t.Run("Test unknown strategy is requested", func(t *testing.T) {
@@ -156,7 +128,7 @@ func TestRouter__InitAccountValidation(t *testing.T) {
 
 		want := &validationResponse{
 			VendorResponse: map[string]string{
-				"test": "ok",
+				"result": "success",
 			},
 		}
 
@@ -166,6 +138,36 @@ func TestRouter__InitAccountValidation(t *testing.T) {
 			t.Errorf(diff)
 		}
 	})
+
+	t.Run("Test micro-deposits strategy", func(t *testing.T) {
+		paygateClient := &paygate.MockClient{}
+
+		strategies := map[validator.StrategyKey]validator.Strategy{
+			validator.StrategyKey{"micro-deposits", "moov"}: microdeposits.NewStrategy(paygateClient),
+		}
+
+		// build request with strategy params
+		params := map[string]string{
+			"strategy": "micro-deposits",
+		}
+
+		var buf bytes.Buffer
+		err := json.NewEncoder(&buf).Encode(params)
+		require.NoError(t, err)
+		body := bytes.NewReader(buf.Bytes())
+
+		w := httptest.NewRecorder()
+		req, err := http.NewRequest("POST", "/", body)
+		require.NoError(t, err)
+		req.Header.Set("Content-Type", "application/json")
+		req = mux.SetURLVars(req, map[string]string{"customerID": customerID, "accountID": acc.AccountID})
+
+		handler := initAccountValidation(log.NewNopLogger(), repo, strategies)
+		handler(w, req)
+
+		require.Equal(t, http.StatusOK, w.Code)
+	})
+
 }
 
 func TestRouter__CompleteAccountValidation(t *testing.T) {
@@ -173,21 +175,13 @@ func TestRouter__CompleteAccountValidation(t *testing.T) {
 	repo := setupTestAccountRepository(t)
 
 	strategies := map[validator.StrategyKey]validator.Strategy{
-		validator.StrategyKey{"test", "moov"}: validator.TestStrategy(),
+		validator.StrategyKey{"test", "moov"}: testvalidator.NewStrategy(),
 	}
-
-	// create account
-	acc, err := repo.createCustomerAccount(customerID, userID, &createAccountRequest{
-		AccountNumber: "123",
-		RoutingNumber: "987654320",
-		Type:          client.CHECKING,
-	})
-	require.NoError(t, err)
 
 	t.Run("Test when account is validated already", func(t *testing.T) {
 		acc, err := repo.createCustomerAccount(customerID, userID, &createAccountRequest{
-			AccountNumber: "1234",
-			RoutingNumber: "987654321",
+			AccountNumber: "123",
+			RoutingNumber: "987654320",
 			Type:          client.CHECKING,
 		})
 		require.NoError(t, err)
@@ -195,8 +189,42 @@ func TestRouter__CompleteAccountValidation(t *testing.T) {
 		err = repo.updateAccountStatus(acc.AccountID, admin.VALIDATED)
 		require.NoError(t, err)
 
-		params := map[string]string{
+		// build request for test strategy
+		params := map[string]interface{}{
 			"strategy": "test",
+			"vendor_request": map[string]string{
+				"result": "success",
+			},
+		}
+
+		var buf bytes.Buffer
+		err = json.NewEncoder(&buf).Encode(params)
+		require.NoError(t, err)
+		body := bytes.NewReader(buf.Bytes())
+
+		w := httptest.NewRecorder()
+		req, err := http.NewRequest("POST", "/", body)
+		require.NoError(t, err)
+		req.Header.Set("Content-Type", "application/json")
+		req = mux.SetURLVars(req, map[string]string{"customerID": customerID, "accountID": acc.AccountID})
+
+		handler := completeAccountValidation(log.NewNopLogger(), repo, strategies)
+		handler(w, req)
+
+		// require.Equal(t, http.StatusBadRequest, w.Code)
+		require.Contains(t, w.Body.String(), fmt.Sprintf("expected accountID=%s status to be 'none'", acc.AccountID))
+	})
+
+	t.Run("Test unknown strategy is requested", func(t *testing.T) {
+		acc, err := repo.createCustomerAccount(customerID, userID, &createAccountRequest{
+			AccountNumber: "1236",
+			RoutingNumber: "987654323",
+			Type:          client.CHECKING,
+		})
+		require.NoError(t, err)
+
+		params := map[string]string{
+			"strategy": "unknown",
 		}
 
 		var buf bytes.Buffer
@@ -214,10 +242,66 @@ func TestRouter__CompleteAccountValidation(t *testing.T) {
 		handler(w, req)
 
 		require.Equal(t, http.StatusBadRequest, w.Code)
-		require.Contains(t, w.Body.String(), fmt.Sprintf("expected accountID=%s status to be 'none'", acc.AccountID))
+		require.Contains(t, w.Body.String(), "strategy unknown for vendor moov was not found")
 	})
 
-	t.Run("Test CompleteAccountValidation with micro-deposits strategy", func(t *testing.T) {
+	t.Run("Test 'test' strategy", func(t *testing.T) {
+		acc, err := repo.createCustomerAccount(customerID, userID, &createAccountRequest{
+			AccountNumber: "1234",
+			RoutingNumber: "987654321",
+			Type:          client.CHECKING,
+		})
+		require.NoError(t, err)
+
+		// build request for test strategy
+		params := map[string]interface{}{
+			"strategy": "test",
+			"vendor_request": map[string]string{
+				"result": "success",
+			},
+		}
+
+		var buf bytes.Buffer
+		err = json.NewEncoder(&buf).Encode(params)
+		require.NoError(t, err)
+		body := bytes.NewReader(buf.Bytes())
+
+		w := httptest.NewRecorder()
+		req, err := http.NewRequest("POST", "/", body)
+		require.NoError(t, err)
+		req.Header.Set("Content-Type", "application/json")
+		req = mux.SetURLVars(req, map[string]string{"customerID": customerID, "accountID": acc.AccountID})
+
+		handler := completeAccountValidation(log.NewNopLogger(), repo, strategies)
+		handler(w, req)
+
+		require.Equal(t, http.StatusOK, w.Code)
+
+		type validationResponse struct {
+			VendorResponse map[string]string `json:"vendor_response"`
+		}
+
+		want := &validationResponse{
+			VendorResponse: map[string]string{
+				"result": "success",
+			},
+		}
+
+		got := &validationResponse{}
+		json.NewDecoder(w.Body).Decode(got)
+		if diff := cmp.Diff(got, want); len(diff) != 0 {
+			t.Errorf(diff)
+		}
+	})
+
+	t.Run("Test micro-deposits strategy", func(t *testing.T) {
+		acc, err := repo.createCustomerAccount(customerID, userID, &createAccountRequest{
+			AccountNumber: "12345",
+			RoutingNumber: "987654322",
+			Type:          client.CHECKING,
+		})
+		require.NoError(t, err)
+
 		paygateClient := &paygate.MockClient{
 			Micro: &payclient.MicroDeposits{
 				MicroDepositID: base.ID(),
@@ -239,7 +323,7 @@ func TestRouter__CompleteAccountValidation(t *testing.T) {
 		}
 
 		var buf bytes.Buffer
-		err := json.NewEncoder(&buf).Encode(params)
+		err = json.NewEncoder(&buf).Encode(params)
 		require.NoError(t, err)
 		body := bytes.NewReader(buf.Bytes())
 
