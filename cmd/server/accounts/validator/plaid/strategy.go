963135d7e67b68529fc61f7e87954c16a5dcf76b
Pavel Gabriel <alovak@gmail.com>
2020-08-31 16:56:41 +0300

    add account validation with Plaid

---
+++ cmd/server/accounts/validator/plaid/strategy.go
@@ -0,0 +1,105 @@
+package plaid
+
+import (
+	"fmt"
+	"net/http"
+
+	"github.com/mitchellh/mapstructure"
+	"github.com/moov-io/customers/client"
+	"github.com/moov-io/customers/cmd/server/accounts/validator"
+	"github.com/plaid/plaid-go/plaid"
+)
+
+type plaidStrategy struct {
+	client         *plaid.Client
+	linkClientName string
+}
+
+type StrategyOptions struct {
+	ClientID    string
+	Secret      string
+	Environment string
+	ClientName  string
+}
+
+var environments = map[string]plaid.Environment{
+	"sandbox":     plaid.Sandbox,
+	"development": plaid.Development,
+	"production":  plaid.Production,
+}
+
+func NewStrategy(options StrategyOptions) (validator.Strategy, error) {
+	env, found := environments[options.Environment]
+	if !found {
+		return nil, fmt.Errorf("unsupported environment %s is specified for Plaid", options.Environment)
+	}
+
+	plaidOptions := plaid.ClientOptions{
+		options.ClientID,
+		options.Secret,
+		env,
+		&http.Client{},
+	}
+
+	client, err := plaid.NewClient(plaidOptions)
+	if err != nil {
+		return nil, err
+	}
+
+	return &plaidStrategy{
+		client:         client,
+		linkClientName: options.ClientName,
+	}, nil
+}
+
+func (s *plaidStrategy) InitAccountValidation(userID, accountID, customerID string) (*validator.VendorResponse, error) {
+	tokenResponse, err := s.client.CreateLinkToken(plaid.LinkTokenConfigs{
+		User: &plaid.LinkTokenUser{
+			ClientUserID: customerID,
+		},
+		ClientName:   s.linkClientName,
+		Products:     []string{"auth"},
+		CountryCodes: []string{"US"},
+		Language:     "en",
+	})
+	if err != nil {
+		return nil, err
+	}
+
+	return &validator.VendorResponse{
+		"link_token": tokenResponse.LinkToken,
+		"expiration": tokenResponse.Expiration,
+	}, nil
+}
+
+type completeAccountValidationRequest struct {
+	PublicToken string `json:"public_token" mapstructure:"public_token"`
+}
+
+func (s *plaidStrategy) CompleteAccountValidation(userID, customerID string, account *client.Account, accountNumber string, request *validator.VendorRequest) (*validator.VendorResponse, error) {
+	input := &completeAccountValidationRequest{}
+	if err := mapstructure.Decode(request, input); err != nil {
+		return nil, fmt.Errorf("unable to parse request params: %v", err)
+	}
+
+	tokenResponse, err := s.client.ExchangePublicToken(input.PublicToken)
+	if err != nil {
+		return nil, err
+	}
+
+	authResponse, err := s.client.GetAuth(tokenResponse.AccessToken)
+	if err != nil {
+		return nil, err
+	}
+
+	// look for account number and routing number in Plaid results
+	for _, acc := range authResponse.Numbers.ACH {
+		if acc.Account == accountNumber && acc.Routing == account.RoutingNumber {
+			return &validator.VendorResponse{
+				"result": "validated",
+			}, nil
+		}
+	}
+
+	return nil, fmt.Errorf("failed to validate account=%s", account.AccountID)
+}
---
+++ cmd/server/accounts/validator/plaid/strategy_test.go
@@ -0,0 +1,79 @@
+package plaid
+
+import (
+	"fmt"
+	"net/http"
+	"os"
+	"testing"
+
+	"github.com/moov-io/base"
+	"github.com/moov-io/customers/client"
+	"github.com/moov-io/customers/cmd/server/accounts/validator"
+	"github.com/plaid/plaid-go/plaid"
+	"github.com/stretchr/testify/require"
+)
+
+func TestStrategy(t *testing.T) {
+	if os.Getenv("PLAID_CLIENT_ID") == "" {
+		t.Skip("No configuration found for Plaid")
+	}
+
+	options := StrategyOptions{
+		os.Getenv("PLAID_CLIENT_ID"),
+		os.Getenv("PLAID_SECRET"),
+		"sandbox",
+		os.Getenv("PLAID_CLIENT_NAME"),
+	}
+
+	strategy, err := NewStrategy(options)
+	require.NoError(t, err)
+
+	customerID, userID, accountID := base.ID(), base.ID(), base.ID()
+
+	t.Run("Test InitAccountValidation", func(t *testing.T) {
+		initResponse, err := strategy.InitAccountValidation(userID, accountID, customerID)
+		require.NoError(t, err)
+		fmt.Println(initResponse)
+
+		require.Contains(t, (*initResponse)["link_token"], "link-sandbox-")
+	})
+
+	t.Run("Test CompleteAccountValidation", func(t *testing.T) {
+		// In order to test CompleteAccountValidation we need to obtain
+		// public token. In production we receive it from Plaid Link (widget)
+		// for test purposes we will use Plaid /sandbox/public_token/create
+		// to create it.
+		// More information here: https://plaid.com/docs/api/2017-03-08/#sandbox-institutions
+		var (
+			sandboxInstitution = "ins_109508"
+			testProducts       = []string{"auth"}
+		)
+
+		var testClient, _ = plaid.NewClient(plaid.ClientOptions{
+			os.Getenv("PLAID_CLIENT_ID"),
+			os.Getenv("PLAID_SECRET"),
+			plaid.Sandbox,
+			&http.Client{},
+		})
+
+		sandboxResp, err := testClient.CreateSandboxPublicToken(sandboxInstitution, testProducts)
+		require.NoError(t, err)
+
+		request := &validator.VendorRequest{
+			"public_token": sandboxResp.PublicToken,
+		}
+
+		// create account that corresponds to Plaid sandbox account
+		account := &client.Account{
+			AccountID:     "xxx",
+			RoutingNumber: "011401533",
+		}
+		accountNumber := "1111222233330000"
+
+		response, err := strategy.CompleteAccountValidation(userID, customerID, account, accountNumber, request)
+		require.NoError(t, err)
+		fmt.Println(response)
+
+		require.Contains(t, (*response)["result"], "validated")
+	})
+}
