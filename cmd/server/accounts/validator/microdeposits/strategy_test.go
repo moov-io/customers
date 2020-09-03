9748b3d08d927531d22a73f90a0c1d989bc1ed8c
Pavel Gabriel <alovak@gmail.com>
2020-09-03 17:40:34 +0300

    return error if micro-deposits were created before

--- cmd/server/accounts/validator/microdeposits/strategy_test.go
+++ cmd/server/accounts/validator/microdeposits/strategy_test.go
@@ -17,12 +17,27 @@ import (
 )
 
 func TestInitAccountValidation(t *testing.T) {
-	paygateClient := &paygate.MockClient{}
-	strategy := NewStrategy(paygateClient)
+	t.Run("When no micro-desposits was created before", func(t *testing.T) {
+		paygateClient := &paygate.MockClient{}
+		strategy := NewStrategy(paygateClient)
 
-	response, err := strategy.InitAccountValidation("userID", "accountID", "customerID")
-	require.NoError(t, err)
-	require.Equal(t, &validator.VendorResponse{"result": "initiated"}, response)
+		response, err := strategy.InitAccountValidation("userID", "accountID", "customerID")
+		require.NoError(t, err)
+		require.Equal(t, &validator.VendorResponse{"result": "initiated"}, response)
+	})
+
+	t.Run("When micro-desposits was created before", func(t *testing.T) {
+		paygateClient := &paygate.MockClient{
+			Micro: &payclient.MicroDeposits{
+				MicroDepositID: base.ID(),
+				Amounts:        []string{"USD 0.03", "USD 0.07"},
+				Status:         payclient.PROCESSED,
+			},
+		}
+		strategy := NewStrategy(paygateClient)
+		_, err := strategy.InitAccountValidation("userID", "accountID", "customerID")
+		require.Error(t, err, "micro-deposits were already created for accountID=accountID")
+	})
 }
 
 func TestCompleteAccountValidation(t *testing.T) {
