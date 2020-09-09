13cd7c188504d213da8fd1bdc3ab7cf9eca7990e
Pavel Gabriel <alovak@gmail.com>
2020-09-09 13:04:54 +0300

    add Validations repository (sqlite only)

---
+++ cmd/server/accounts/validator/repository_test.go
@@ -0,0 +1,50 @@
+package validator
+
+import (
+	"testing"
+
+	"github.com/moov-io/base"
+	"github.com/moov-io/customers/internal/database"
+	"github.com/stretchr/testify/require"
+)
+
+func TestRepository(t *testing.T) {
+	repo := createTestRepository(t)
+	defer repo.Close()
+
+	accountID := base.ID()
+
+	validation := &Validation{
+		AccountID: accountID,
+		Status:    StatusInit,
+		Strategy:  "instant",
+		Vendor:    "mx",
+	}
+
+	err := repo.CreateValidation(validation)
+	require.NoError(t, err)
+	require.NotEmpty(t, validation.ValidationID)
+
+	validation, err = repo.GetValidation(accountID, validation.ValidationID)
+	require.NoError(t, err)
+	require.Equal(t, "instant", validation.Strategy)
+	require.Equal(t, "mx", validation.Vendor)
+	require.NotEmpty(t, validation.CreatedAt)
+	require.Equal(t, validation.CreatedAt, validation.UpdatedAt)
+
+	validation.Strategy = "micro-deposits"
+	validation.Vendor = "moov"
+	validation.Status = StatusCompleted
+
+	err = repo.UpdateValidation(validation)
+	require.NoError(t, err)
+	require.NotEqual(t, validation.CreatedAt, validation.UpdatedAt)
+
+}
+
+func createTestRepository(t *testing.T) *sqlRepository {
+	t.Helper()
+
+	db := database.CreateTestSqliteDB(t)
+	return &sqlRepository{db.DB}
+}
