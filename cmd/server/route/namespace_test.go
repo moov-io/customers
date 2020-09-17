9d596965c855df2bd1bcfee1ca613b2cfada5467
Adam Shannon <adamkshannon@gmail.com>
2020-09-16 22:42:33 -0500

    route: test when X-Namespace is missing

--- cmd/server/route/namespace_test.go
+++ cmd/server/route/namespace_test.go
@@ -5,6 +5,7 @@
 package route
 
 import (
+	"net/http"
 	"net/http/httptest"
 	"testing"
 )
@@ -16,5 +17,18 @@ func TestRoute__GetNamespace(t *testing.T) {
 
 	if ns := GetNamespace(w, req); ns != "foo" {
 		t.Errorf("unexpected ns: %v", ns)
+	}
+}
+
+func TestRoute__GetNamespaceMissing(t *testing.T) {
+	w := httptest.NewRecorder()
+	req := httptest.NewRequest("GET", "/ping", nil)
+
+	if ns := GetNamespace(w, req); ns != "" {
+		t.Errorf("unexpected ns: %v", ns)
+	}
+
+	if w.Code != http.StatusBadRequest {
+		t.Errorf("bogus HTTP status: %d", w.Code)
 	}
 }
