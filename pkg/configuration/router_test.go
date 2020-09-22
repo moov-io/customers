21949bf5027c5fd535f79dcb40352e8835ebbb55
Adam Shannon <adamkshannon@gmail.com>
2020-09-22 13:40:58 -0500

    configuration: more tests around error conditions

--- pkg/configuration/router_test.go
+++ pkg/configuration/router_test.go
@@ -7,14 +7,17 @@ package configuration
 import (
 	"bytes"
 	"encoding/json"
+	"errors"
 	"net/http"
 	"net/http/httptest"
 	"testing"
 
-	"github.com/go-kit/kit/log"
-	"github.com/gorilla/mux"
 	"github.com/moov-io/base"
 	"github.com/moov-io/customers/pkg/client"
+
+	"github.com/go-kit/kit/log"
+	"github.com/gorilla/mux"
+	"github.com/stretchr/testify/require"
 )
 
 func TestRouterGet(t *testing.T) {
@@ -34,9 +37,7 @@ func TestRouterGet(t *testing.T) {
 	router.ServeHTTP(w, req)
 	w.Flush()
 
-	if w.Code != http.StatusOK {
-		t.Errorf("bogus HTTP status: %v", w.Code)
-	}
+	require.Equal(t, w.Code, http.StatusOK)
 
 	var response client.NamespaceConfiguration
 	if err := json.NewDecoder(w.Body).Decode(&response); err != nil {
@@ -48,6 +49,42 @@ func TestRouterGet(t *testing.T) {
 	if response.PrimaryAccount == "" {
 		t.Errorf("PrimaryAccount=%q", response.PrimaryAccount)
 	}
+}
+
+func TestRouterGetErr(t *testing.T) {
+	repo := &mockRepository{
+		err: errors.New("bad error"),
+	}
+
+	req := httptest.NewRequest("GET", "/configuration/customers", nil)
+	req.Header.Set("X-Namespace", "moov")
+	w := httptest.NewRecorder()
+
+	router := mux.NewRouter()
+	RegisterRoutes(log.NewNopLogger(), router, repo)
+	router.ServeHTTP(w, req)
+	w.Flush()
+
+	require.Equal(t, w.Code, http.StatusBadRequest)
+}
+
+func TestRouterGetMissing(t *testing.T) {
+	repo := &mockRepository{
+		cfg: &client.NamespaceConfiguration{
+			LegalEntity:    base.ID(),
+			PrimaryAccount: base.ID(),
+		},
+	}
+
+	req := httptest.NewRequest("GET", "/configuration/customers", nil)
+	w := httptest.NewRecorder()
+
+	router := mux.NewRouter()
+	RegisterRoutes(log.NewNopLogger(), router, repo)
+	router.ServeHTTP(w, req)
+	w.Flush()
+
+	require.Equal(t, w.Code, http.StatusBadRequest)
 }
 
 func TestRouterUpdate(t *testing.T) {
@@ -73,9 +110,7 @@ func TestRouterUpdate(t *testing.T) {
 	router.ServeHTTP(w, req)
 	w.Flush()
 
-	if w.Code != http.StatusOK {
-		t.Errorf("bogus HTTP status: %v", w.Code)
-	}
+	require.Equal(t, w.Code, http.StatusOK)
 
 	var response client.NamespaceConfiguration
 	if err := json.NewDecoder(w.Body).Decode(&response); err != nil {
@@ -87,4 +122,52 @@ func TestRouterUpdate(t *testing.T) {
 	if response.PrimaryAccount == "" {
 		t.Errorf("PrimaryAccount=%q", response.PrimaryAccount)
 	}
+}
+
+func TestRouterUpdateErr(t *testing.T) {
+	repo := &mockRepository{
+		err: errors.New("bad error"),
+	}
+
+	var body bytes.Buffer
+	json.NewEncoder(&body).Encode(&client.NamespaceConfiguration{
+		LegalEntity:    base.ID(),
+		PrimaryAccount: base.ID(),
+	})
+
+	req := httptest.NewRequest("PUT", "/configuration/customers", &body)
+	req.Header.Set("X-Namespace", "moov")
+	w := httptest.NewRecorder()
+
+	router := mux.NewRouter()
+	RegisterRoutes(log.NewNopLogger(), router, repo)
+	router.ServeHTTP(w, req)
+	w.Flush()
+
+	require.Equal(t, w.Code, http.StatusBadRequest)
+}
+
+func TestRouterUpdateMissing(t *testing.T) {
+	repo := &mockRepository{
+		cfg: &client.NamespaceConfiguration{
+			LegalEntity:    base.ID(),
+			PrimaryAccount: base.ID(),
+		},
+	}
+
+	var body bytes.Buffer
+	json.NewEncoder(&body).Encode(&client.NamespaceConfiguration{
+		LegalEntity:    base.ID(),
+		PrimaryAccount: base.ID(),
+	})
+
+	req := httptest.NewRequest("PUT", "/configuration/customers", &body)
+	w := httptest.NewRecorder()
+
+	router := mux.NewRouter()
+	RegisterRoutes(log.NewNopLogger(), router, repo)
+	router.ServeHTTP(w, req)
+	w.Flush()
+
+	require.Equal(t, w.Code, http.StatusBadRequest)
 }
