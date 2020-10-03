16e0ac41b0590ff7042490e1859c179950b4da81
Adam Shannon <adamkshannon@gmail.com>
2020-10-03 12:34:32 -0500

    api,client: update shared Error model

--- pkg/client/api_configuration.go
+++ pkg/client/api_configuration.go
@@ -11,11 +11,10 @@ package client
 
 import (
 	_context "context"
+	"github.com/antihax/optional"
 	_ioutil "io/ioutil"
 	_nethttp "net/http"
 	_neturl "net/url"
-
-	"github.com/antihax/optional"
 )
 
 // Linger please
