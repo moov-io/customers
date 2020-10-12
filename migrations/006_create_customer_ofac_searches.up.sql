159097cee03de5d2583701078e916b75aebedfbc
Pavel Gabriel <alovak@gmail.com>
2020-10-12 13:29:57 +0200

    add blocked into ofac_search table

--- migrations/006_create_customer_ofac_searches.up.sql
+++ migrations/006_create_customer_ofac_searches.up.sql
@@ -4,5 +4,6 @@ create table if not exists customer_ofac_searches(
   sdn_name varchar(40), 
   sdn_type integer, 
   percentage_match double precision (5, 2), 
+  blocked boolean,
   created_at datetime
 );
