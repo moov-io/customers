27207a7c094058f34fc17fd4dc53301a87aee711
Pavel Gabriel <alovak@gmail.com>
2020-10-12 17:02:42 +0200

    extract adding 'blocked' table field into separate migration file

--- migrations/006_create_customer_ofac_searches.up.sql
+++ migrations/006_create_customer_ofac_searches.up.sql
@@ -4,6 +4,5 @@ create table if not exists customer_ofac_searches(
   sdn_name varchar(40), 
   sdn_type integer, 
   percentage_match double precision (5, 2), 
-  blocked boolean,
   created_at datetime
 );
---
+++ migrations/016_change_customer_ofac_searches_add_blocked.up.sql
@@ -0,0 +1,1 @@
+alter table customer_ofac_searches add column blocked boolean;
