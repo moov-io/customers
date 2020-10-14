d11e7fbdd3128e380b358957f15e7b834114eaaa
Pavel Gabriel <alovak@gmail.com>
2020-10-14 12:18:00 +0200

    restore env variables after test

--- pkg/config/config_test.go
+++ pkg/config/config_test.go
@@ -10,24 +10,27 @@ import (
 
 func TestConfig(t *testing.T) {
 	t.Run("When DATABASE_TYPE is not set", func(t *testing.T) {
-		os.Setenv("DATABASE_TYPE", "")
+		setenv(t, "DATABASE_TYPE", "")
 
 		conf := New()
-		conf.Load()
+		err := conf.Load()
+
+		require.NoError(t, err)
 
 		require.Equal(t, conf.Database.SqlLite.Path, "customers.db")
 	})
 
 	t.Run("When DATABASE_TYPE is set to mysql", func(t *testing.T) {
-		os.Setenv("DATABASE_TYPE", "mysql")
-		os.Setenv("MYSQL_USER", "user")
-		os.Setenv("MYSQL_PASSWORD", "password")
-		os.Setenv("MYSQL_ADDRESS", "tcp(localhost:1234)")
-		os.Setenv("MYSQL_DATABASE", "test")
+		setenv(t, "DATABASE_TYPE", "mysql")
+		setenv(t, "MYSQL_USER", "user")
+		setenv(t, "MYSQL_PASSWORD", "password")
+		setenv(t, "MYSQL_ADDRESS", "tcp(localhost:1234)")
+		setenv(t, "MYSQL_DATABASE", "test")
 
 		conf := New()
-		conf.Load()
+		err := conf.Load()
 
+		require.NoError(t, err)
 		require.Equal(t, "test", conf.Database.DatabaseName)
 		want := &database.MySqlConfig{
 			Address:  "tcp(localhost:1234)",
@@ -36,4 +39,19 @@ func TestConfig(t *testing.T) {
 		}
 		require.Equal(t, want, conf.Database.MySql)
 	})
+}
+
+// setenv restores env variables after test
+func setenv(t *testing.T, key, val string) {
+	if oldVal, found := os.LookupEnv(key); found {
+		t.Cleanup(func() {
+			os.Setenv(key, oldVal)
+		})
+	} else {
+		t.Cleanup(func() {
+			os.Unsetenv(key)
+		})
+	}
+
+	os.Setenv(key, val)
 }
